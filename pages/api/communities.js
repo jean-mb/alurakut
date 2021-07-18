52246b783f772338aae370ce5922826c237f9bd5
Jean <jeanfelipe0610@gmail.com>
2021-07-18 20:06:29 -0300

    update

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -1,17 +1,10 @@
 import { SiteClient } from 'datocms-client'
 
 export default async function receiveRequests(request, response) {
-<<<<<<< HEAD
   if (request.method === 'POST') {
     const TOKEN = 'b651593414a5cf13a74f5e6847d094'
     const client = new SiteClient(TOKEN)
 
-=======
-  if(request.method === 'POST') {
-    const TOKEN = 'b651593414a5cf13a74f5e6847d094';
-    const client = new SiteClient(TOKEN);
-  
->>>>>>> 4d3ff1db0e7860a9865af39d45d032eb23c1bccf
     const registerCreated = await client.items.create({
       itemType: '968409',
       ...request.body
